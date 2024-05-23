f95bffb198097461b60661c3936f526bb8b21200
Franklin <franklinjaradev@gmail.com>
2024-05-23 02:14:11 -0400

    chore: adding source graph cody ai

--- lua/plugins/source-graph.lua
+++ lua/plugins/source-graph.lua
@@ -4,7 +4,7 @@ return {
   {
     "sourcegraph/sg.nvim",
     dependencies = {
-      "nvim-lua/plenary.nvim",
+      "nvim-lua/plenary.nvim", --[[ "nvim-telescope/telescope.nvim ]]
       config = function()
         require("sg").setup({})
       end,
